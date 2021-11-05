bae51562797f6af6e679d9ce83f4152100313510
Kailin <kailin.zhou@student.uts.edu.au>
2021-11-05 21:38:27 +1100

    unittest updated

--- app/test/test_data.py
+++ app/test/test_data.py
@@ -18,7 +18,7 @@ class TestGet_n_Rows(unittest.TestCase):
     def test(self):
        expected = 1000
        result = data.get_n_rows()
-       self.assertEqual(result, expected) 
+       self.assertEqual(result, expected)
 
 class TestGet_n_Cols(unittest.TestCase):
     def test(self):
@@ -30,6 +30,45 @@ class TestColList(unittest.TestCase):
     def test(self):
        expected = ['datetime', 'lat', 'lon', 'base']
        result = data.get_cols_list()
+       self.assertEqual(result, expected) 
+
+class TestGetColType(unittest.TestCase):
+    def test(self):
+       expected = {'base': 'object',
+    'datetime': 'datetime64[ns]',
+    'lat': 'float64',
+    'lon': 'float64'}
+       result = data.get_cols_dtype()
+       self.assertEqual(result, expected) 
+
+class TestDuplicates(unittest.TestCase):
+    def test(self):
+       expected = df_pd.duplicated().sum()
+       result = data.get_n_duplicates()
+       self.assertEqual(result, expected) 
+
+class TestMissings(unittest.TestCase):
+    def test(self):
+       expected = df_pd.shape[0]-df_pd.dropna().shape[0]
+       result = data.get_n_missing()
+       self.assertEqual(result, expected) 
+
+class TestGetNumericCol(unittest.TestCase):
+    def test(self):
+       expected = ['lat', 'lon']
+       result = data.get_numeric_columns()
+       self.assertEqual(result, expected) 
+
+class TestGetTextCol(unittest.TestCase):
+    def test(self):
+       expected = ['base']
+       result = data.get_text_columns()
+       self.assertEqual(result, expected) 
+
+class TestGetDateCol(unittest.TestCase):
+    def test(self):
+       expected = ['datetime']
+       result = data.get_date_columns()
        self.assertEqual(result, expected) 
 
 if __name__ == '__main__':
